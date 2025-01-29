abadb5a45d5d3f3d6287773ed41c3f1f8392dc73
the-official-zombia-experts-team <qtdz@genocide.fun>
2025-01-29 21:48:06 +0000

    Delete mymiddleware.js

--- mymiddleware.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    console.log(`[${new Date()}] User with UserAgent ${req.get("User-Agent")} requested [${req.method}] ${req.originalUrl}`);
-    next();
-};
-
-module.exports = {
-    logger
-};
